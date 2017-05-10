7cb99b2d2a3670c86a83e61cbe2e20a281fe72a7
Uriel Duran <uduran12@gmail.com>
2017-05-10 18:26:52 -0500

    Long notation conditional

--- Conditionals/view.index.php
+++ Conditionals/view.index.php
@@ -11,18 +11,6 @@
 
 
         <h1>Task for the day.</h1>
-        <ul>
-            <?php foreach($task as $heading => $value) : ?>
-
-                <li> 
-                    <!--ucwords capitalizes first letter.-->
-                    <strong> <?= ucwords($heading); ?>: </strong> <?= $value ?>
-                </li>
-                
-                <?php endforeach; ?>
-
-        </ul>
-
 
         <!--OTHER EXAMPLE ON HOW TO DO THIS-->
 
@@ -31,10 +19,33 @@
                 <strong>Name: </strong> <?= $task['title'];?>
             </li>
 
+             <li>
+                <strong>Due Date: </strong> <?= $task['due'];?>
+            </li>
+
+             <li>
+                <strong>Person Responsible: </strong> <?= $task['assigned_to'];?>
+            </li>
+
+
             <!--Output the boolean Example-->
             <li>
+                <strong>Status:</strong>
+                    <?php
+                        if($task['completed']) {
+
+                            echo 'Complete';
+
+                        }else{
+                            echo 'Incomplete';
+                        }
+                    ?>
+
+
+
                 <!--USEFUL: Outputs either 'Complete' or 'Incomplete' based on Boolean value.-->
-                <strong>Status:</strong> <?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+                <!--<strong>Status:</strong> <//?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>-->
+
             </li>
 
 
