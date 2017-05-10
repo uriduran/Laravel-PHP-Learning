6b635a96bd22aea47399359d27c335b5298878b9
Uriel Duran <uduran12@gmail.com>
2017-05-09 21:24:06 -0500

    Assosiative Arrays Tutorial

---
+++ Associative Arrays/index.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <style>
+            header{
+                background: grey;
+                padding: 2em;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <ul>
+            <!--Method One for listing an array. SLOPPY-->
+            <?php 
+                foreach($names as $name){
+                    echo "<li>$name</li>";
+                }
+            ?>
+
+            <!--Method Two Prefered-->
+               <?php foreach($names as $name): ?>
+
+               <li> <?php echo $name; ?> </li>
+
+               <?php endforeach; ?>
+
+        </ul>
+    </body>
+</html>
