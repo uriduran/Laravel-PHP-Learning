86ca847a322b13b8a2ca60a6d2fd9f8a5fd2cf01
Uriel Duran <uduran12@gmail.com>
2017-05-09 21:20:49 -0500

    Commenting out Initial tutorial

--- Arrays/index.php
+++ Arrays/index.php
@@ -8,9 +8,10 @@ $names = [
 ];
 
 //foreach function takes in the array ($names) and references "as $name"
-foreach ($names as $name){ //Starts a small loop that goes through the array.
-    echo $name . ', '; //echoes each item in the array as $name
+    // foreach ($names as $name){ //Starts a small loop that goes through the array.
+    //     echo $name . ', '; //echoes each item in the array as $name
+    // }
 
-}
+require 'index.view.php';
 
 
