8198a681c93c45350c9916c4522f80b5bee3b137
Uriel Duran <uduran12@gmail.com>
2017-05-10 18:48:49 -0500

    Removed blank lines

--- Conditionals/view.index.php
+++ Conditionals/view.index.php
@@ -7,36 +7,26 @@
     </head>
 
     <body>
-
-
-
         <h1>Task for the day.</h1>
 
         <!--OTHER EXAMPLE ON HOW TO DO THIS-->
-
         <ul>
             <li>
                 <strong>Name: </strong> <?= $task['title'];?>
             </li>
-
-             <li>
+            <li>
                 <strong>Due Date: </strong> <?= $task['due'];?>
             </li>
-
-             <li>
+            <li>
                 <strong>Person Responsible: </strong> <?= $task['assigned_to'];?>
             </li>
-
 
             <!--Output the boolean Example-->
             <li>
                 <strong>Status:</strong>
-
                     <?php
                         if($task['completed']) {
-
                             echo '&#9989;';
-
                         }else{
                             echo 'Incomplete';
                         }
@@ -46,25 +36,15 @@
 
                 <!--Can Change it to this so that we are not echoing out a bunch of stuff-->
                 <strong>Status:</strong>
-                
                     <?php if ($task['completed']) : ?>
-
                         &#9989;
-
+                    <?php else : ?>
+                        Wrong
                     <?PHP endif; ?>
 
                 <!--USEFUL: Outputs either 'Complete' or 'Incomplete' based on Boolean value.-->
                 <!--<strong>Status:</strong> <//?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>-->
-
             </li>
-
-
-
         </ul>
-
-
-
-    
     </body>
-
 </html>
