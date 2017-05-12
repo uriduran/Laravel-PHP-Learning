094689c6a8c5ba99bdd524adab88db331e4ff705
Uriel Duran <uduran12@gmail.com>
2017-05-11 21:03:59 -0500

    done

--- 06_Classes/index.php
+++ 06_Classes/index.php
@@ -1,5 +1,57 @@
 <?php
 
+class Task {//An instance of a class is an object.
+
+    //defining properties. Protected cannot be called from outside.
+    public $description;
+    public $completed = false;
+
+    //method if its inside a class otherwise called a function
+    public function __construct($description) 
+    { 
+
+        $this->description = $description;
+    }
+
+        public function complete()
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+
+
+}
+
+
+$tasks = [
+
+    new Task('Go to the store'),
+    new Task('Go to the store2'),
+    new Task('Go to the store3')
+
+];
+
+$tasks[0]->complete();
+
+var_dump($tasks);
+
+
+
+// $task = new Task('Go to the store'); //New task creation
+
+// $task->complete();
+
+// var_dump($task->isComplete()); // check if its complete
+
+
+
+
+require 'view.index.php';
+
 
 
 
--- 06_Classes/view.index.php
+++ 06_Classes/view.index.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Booleans</title>
+        <title>Classes</title>
         <meta charset="UTF-8">
     </head>
+
+        <ul>
+            <?php foreach ($tasks as $task) : ?>
+
+                <li>
+                    <?php if ($task->completed) : ?>
+                        <strike>
+                    <?php endif; ?>
+
+                    <?= $task->description; ?>
+
+                    <?php if ($task->completed) : ?>
+                        <strike>
+                    <?php endif; ?>
+                </li>
+            
+            <?php endforeach ?>
+        </ul>   
 
     <body>
             
