9583241e8d03803722cc643dd68b96258430dca1
Uriel Duran <uduran12@gmail.com>
2017-05-18 16:04:35 -0500

    DB connect to function

---
+++ 07_PDO/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function connectToDb()
+{
+    try {//use a try catch for error catching.
+        return new PDO('mysql:host=localhost;dbname=mytodo', 'root', ''); //new instance of PDO
+    } catch (PDOException $e) {
+        die($e->getMessage); //if connection fails display this. Calls getMessage Function. Gives more details about error.
+    }
+
+    $statement = $pdo->prepare('select * from todos'); //prepared statement but has not exectuted.
+
+    $statement->execute(); //executes SQL statement
+
+// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+//by default it fetches as object and associative arrays
+//this specifies it as object.
+//Stores it as a variable $results
+// fetchAll fetches everything. Can be dangerous if DB calls a lot of data.
+
+    $tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task'); //Fetches into a class called Task
+}
--- 07_PDO/index.php
+++ 07_PDO/index.php
@@ -1,33 +1,14 @@
 <?php
 
+require 'functions.php';
 require 'Task.php';
 
-try {//use a try catch for error catching.
-    $pdo = new PDO('mysql:host=localhost;dbname=mytodo', 'root', ''); //new instance of PDO
-} catch (PDOException $e) {
-    die($e->getMessage); //if connection fails display this. Calls getMessage Function. Gives more details about error.
-}
+$pdo = connectToDb; //result from connectToDb function is saved as pdo
 
-$statement = $pdo->prepare('select * from todos'); //prepared statement but has not exectuted.
-
-$statement->execute(); //executes SQL statement
-
-// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
-//by default it fetches as object and associative arrays
-//this specifies it as object.
-//Stores it as a variable $results
-// fetchAll fetches everything. Can be dangerous if DB calls a lot of data.
-
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task'); //Fetches into a class called Task
 
 var_dump($tasks);
 
 var_dump($tasks[0]->description); //fetches the description column for the 0th array value.$_COOKIE
-
-var_dump($tasks[1]->foobar());
-
-
-
 
 
 require 'index.view.php';
