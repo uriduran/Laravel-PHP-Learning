238e89ccd4ed785483ea8ed527508a8c4668bcb0
Uriel Duran <uduran12@gmail.com>
2017-05-18 18:14:20 -0500

    DB config file created with logic

---
+++ 08_PDO_Refactoring/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'mytodo',
+        'username' => 'root',
+        'password' => '',
+        'connection' => 'mysql:host=localhost',
+        'options' => []
+    ]
+];
--- 08_PDO_Refactoring/database/Connection.php
+++ 08_PDO_Refactoring/database/Connection.php
@@ -2,10 +2,16 @@
 
 class Connection
 {
-    public static function make()
+    public static function make($config)
     {
         try {//use a try catch for error catching.
-            return new PDO('mysql:host=localhost;dbname=mytodo', 'root', ''); //new instance of PDO
+           
+            return new PDO(
+                $config['connection'] . ';dbname=' .$config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
         } catch (PDOException $e) {
             die($e->getMessage); //if connection fails display this. Calls getMessage Function. Gives more details about error.
         }
