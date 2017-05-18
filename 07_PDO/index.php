4342988628e5277a7a8bf3be22b6f03bce73672f
Uriel Duran <uduran12@gmail.com>
2017-05-18 15:37:37 -0500

    comment

--- 07_PDO/index.php
+++ 07_PDO/index.php
@@ -3,14 +3,23 @@
 try {//use a try catch for error catching.
     $pdo = new PDO('mysql:host=localhost;dbname=mytodo', 'root', ''); //new instance of PDO
 } catch (PDOException $e) {
-    die('Could not connect.'); //if connection fails display this.
+    die($e->getMessage); //if connection fails display this.
 }
 
 $statement = $pdo->prepare('select * from todos'); //prepared statement but has not exectuted.
 
 $statement->execute(); //executes SQL statement
 
-var_dump($statement->fetchAll());
+$results = $statement->fetchAll(PDO::FETCH_OBJ);
+//by default it fetches as object and associative arrays
+//this specifies it as object.
+//Stores it as a variable $results
+
+var_dump($results);
+
+var_dump($results[0]->description); //fetches the description column for the 0th array value.$_COOKIE
+
+
 
 
 
