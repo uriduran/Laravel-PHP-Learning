e7aae9fe38967c2f91eb01b2cb196e5425951ad8
Uriel Duran <uduran12@gmail.com>
2017-05-18 15:39:54 -0500

    Copied html view from last lesson

--- 07_PDO/index.view.php
+++ 07_PDO/index.view.php
@@ -1,11 +1,31 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>Classes</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
+
+        <!--It will now fetch from the DB and not from an array-->
+        <ul>
+            <?php foreach ($tasks as $task) : ?>
+
+                <li>
+                
+                    <?php if ($task->completed) : ?>
+                        <strike><?= $task->description; ?></strike>
+                    <?php else : ?>
+
+                    <?= $task->description; ?>
+
+                    <?php endif; ?>
+
+                </li>
+            <?php endforeach ?>
+
+        </ul>   
+
     <body>
-    
+            
     </body>
 </html>
