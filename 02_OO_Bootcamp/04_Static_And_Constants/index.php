1d2e41790b627ce52e7c4947a263a0e233c61f27
Uriel Duran <uduran12@gmail.com>
2017-06-10 11:36:07 -0500

    comments

--- 02_OO_Bootcamp/04_Static_And_Constants/index.php
+++ 02_OO_Bootcamp/04_Static_And_Constants/index.php
@@ -13,3 +13,9 @@ class Math
 //var_dump($math->add(1, 2, 3, 4));
 
 echo Math::add(1, 2, 3, 4);
+
+//No longer reqires an instance.
+//We're accessing add method from anywhere.
+//essentially turned into global function
+//Be careful if the method called other methods.
+//not a good idea.
